4fbf06e4a166242e2b1924d8d0b73bfb95357419
abiduldul <abidul000@gmail.com>
2026-06-22 16:04:02 +0700

    edit files architecture

---
+++ build/mertani-2.5/CMakeFiles/3.28.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.8.0-124-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-124-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/abid/Github/STM32_Low-Level-Programming_VSCode/cmake/toolchain-mertani_2.5.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
